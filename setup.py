050079bcdcfcb2663479b8e8647ebc84d32dab36
LeonardoIalongo <leonardo.ialongo@gmail.com>
2020-09-01 12:44:40 +0200

    Corrected requirements in setup file to install directly for development and prod env

--- setup.py
+++ setup.py
@@ -1,3 +1,17 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(name="graph_ensembles", packages=find_packages())
+setup(
+    name='graph_ensembles',
+    version='0.0.1',
+    py_modules=['graph_ensembles'],
+    package_dir={'': 'src'},
+    install_requires=["numpy>=1.19",
+                      "scipy>=1.5"],
+    extras_require={
+        "dev": ["pytest==6.0.1",
+                "coverage==5.2.1",
+                "pytest-cov==2.10.1",
+                "flake8==3.8.3",
+                "wheel==0.35.1"],
+        },
+    )
