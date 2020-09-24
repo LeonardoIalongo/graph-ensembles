fc2c26e41aec8fae7d66c531ae94d80a328eb2e2
LeonardoIalongo <leonardo.ialongo@gmail.com>
2020-09-24 14:48:25 +0200

    Switched to computing random links rather than margins

--- performance_eval.py
+++ performance_eval.py
@@ -4,7 +4,7 @@ It plots how the computation time evolves with increasing number of links and
 edges. The results are platform dependent but the relative performance of
 improvements to the models should be tested.
 """
-
+import time
 import graph_ensembles as ge
 import numpy as np
 
@@ -12,7 +12,7 @@ import numpy as np
 N = np.logspace(1, 1, num=1, dtype=np.int64)
 
 # Corresponding number of edges
-L = N * 10
+L = N * 2
 
 # Corresponding number of groups
 G = np.floor(N / 3).astype('int')
@@ -20,42 +20,42 @@ G = np.floor(N / 3).astype('int')
 # Corresponding max strength
 W = N * 2
 
+# Initialize computation time list
+comp_time = []
+
 for n, l, g, w in zip(N, L, G, W):
-    # Define non-zero elements of graph marginals
-    out_strength = np.random.randint(low=0, high=2, size=(n, g),
-                                     dtype=np.int64)
-    in_strength = np.random.randint(low=0, high=2, size=(n, g),
-                                    dtype=np.int64)
+    # Define indexes of non-zero elements of adjacency matrix
+    adj = np.zeros((l, 4), dtype=np.int64)
+    adj[:, 0:2] = np.random.randint(low=0, high=n, size=(l, 2), dtype=np.int64)
+    adj[:, 2] = np.random.randint(low=0, high=g, size=l, dtype=np.int64)
+    adj[:, 3] = np.random.randint(low=1, high=w, size=l, dtype=np.int64)
 
-    # Add fitness value
-    out_strength *= np.random.randint(low=1, high=w+1, size=(n, g),
-                                      dtype=np.int64)
-    in_strength *= np.random.randint(low=1, high=w+1, size=(n, g),
-                                     dtype=np.int64)
+    # Compute margins
+    indexes = set()
+    out_strength = np.zeros((n, g))
+    in_strength = np.zeros((n, g))
+    for link in adj:
+        if (link[0], link[1]) not in indexes:
+            indexes.add((link[0], link[1]))
 
-    # Correct difference between total in and out strength per group
-    diff = np.sum(out_strength, axis=0) - np.sum(in_strength, axis=0)
+        out_strength[link[0], link[2]] += link[3]
+        in_strength[link[1], link[2]] += link[3]
 
-    # Distribute difference over all non zero elements
-    for i in range(0, g):
-        x = in_strength[:, i]
-        x[x != 0] += np.floor(diff[i] / len(x[x != 0])).astype('int')
-        x[x < 0] = 0
+    real_l = len(indexes)
 
-    # Give remainder to first element
-    diff = np.sum(out_strength, axis=0) - np.sum(in_strength, axis=0)
-    for i in range(0, g):
-        row_mask = np.argmax(in_strength[:, i] > diff[i])
-        in_strength[row_mask, i] += diff[i]
-
+    # Check that the in and out strengths match per group
     assert all(np.sum(out_strength, axis=0) == np.sum(in_strength, axis=0))
 
     # Create model
     model = ge.VectorFitnessModel(out_strength,
                                   in_strength,
-                                  l)
+                                  real_l)
 
     # Solve and check that solution is admissible
+    start = time.perf_counter()
+    model.solve()
     exp_num_links = model.probability_matrix.sum()
-    print(l)
-    print(exp_num_links)
+    comp_time.append(time.perf_counter() - start)
+    np.testing.assert_almost_equal(real_l, exp_num_links, decimal=5)
+
+print(comp_time)
