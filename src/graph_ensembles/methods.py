2974befa6d1ebd954a9087d3767efcec05c0681c
LeonardoIalongo <leonardo.ialongo@gmail.com>
2020-10-19 16:00:48 +0200

    All tests currently working for stripe model, many jits not working

--- src/graph_ensembles/methods.py
+++ src/graph_ensembles/methods.py
@@ -3,7 +3,7 @@ from numba import jit
 from numba import prange
 
 
-#@jit(nopython=True, parallel=True)
+# @jit(nopython=True, parallel=True)
 def prob_matrix_stripe_one_z(out_strength, in_strength, z, N):
     """ Compute the probability matrix of the stripe fitness model given the
     single parameter z controlling for the density.
@@ -26,7 +26,7 @@ def prob_matrix_stripe_one_z(out_strength, in_strength, z, N):
     return 1 - p
 
 
-@jit(nopython=True, parallel=True)
+# @jit(nopython=True, parallel=True)
 def prob_array_stripe_one_z(out_strength, in_strength, z, N, G):
     """ Compute the probability array of the stripe fitness model given the
     single parameter z controlling for the density.
@@ -215,9 +215,8 @@ def vector_fitness_prob_array_block_one_z(out_strength, in_strength,
     return p
 
 
-#@jit(forceobj=True, parallel=True)
-def expected_links_block_one_z(out_strength, in_strength,
-                                         z):
+# @jit(forceobj=True, parallel=True)
+def expected_links_block_one_z(out_strength, in_strength, z):
     """
     Function computing the expeceted number of links, under the Cimi
     stripe model, given the parameter z controlling for the density.
@@ -244,10 +243,11 @@ def expected_links_block_one_z(out_strength, in_strength,
 
 @jit(nopython=True)
 def assign_weights_cimi_block_one_z(p, out_strength, in_strength,
-                                     N, strengths_block, expected=True):
-    """Function returning the weighted adjacency matrix of the Cimi block model
-    with just one global parameter z controlling for the density. Depending on the value of 
-    "expected" the weighted adjacency matrix can be the expceted one or just an ensemble realisation.
+                                    N, strengths_block, expected=True):
+    """Function returning the weighted adjacency matrix of the Cimi block
+    model with just one global parameter z controlling for the density.
+    Depending on the value of "expected" the weighted adjacency matrix can be
+    the expceted one or just an ensemble realisation.
 
     Parameters
     ----------
@@ -272,7 +272,7 @@ def assign_weights_cimi_block_one_z(p, out_strength, in_strength,
     all indices. Consider allowing for sparse matrices in case of groups and
     to avoid iteration over all indices.
     """
-    
+
     W = np.zeros((N, N), dtype=np.float64)
     if expected:
         for i in np.arange(out_strength.shape[0]):
@@ -286,10 +286,10 @@ def assign_weights_cimi_block_one_z(p, out_strength, in_strength,
                 sect_in = int(in_strength[j, 2])
                 s_in = in_strength[j, 3]
                 if ((ind_out != ind_in) & (sect_out == sect_node_j) &
-               (sect_in == sect_node_i)):
-                    tot_w = strengths_block[sect_node_i,sect_node_j]
+                   (sect_in == sect_node_i)):
+                    tot_w = strengths_block[sect_node_i, sect_node_j]
                     tmp = s_out*s_in
-                    W[ind_out, ind_in] = (tmp)/(tot_w * p[ind_out,ind_in])
+                    W[ind_out, ind_in] = (tmp)/(tot_w * p[ind_out, ind_in])
     else:
         for i in np.arange(out_strength.shape[0]):
             ind_out = int(out_strength[i, 0])
@@ -302,7 +302,7 @@ def assign_weights_cimi_block_one_z(p, out_strength, in_strength,
                 sect_in = int(in_strength[j, 2])
                 s_in = in_strength[j, 3]
                 if ((ind_out != ind_in) & (sect_out == sect_node_j) &
-               (sect_in == sect_node_i)):
+                   (sect_in == sect_node_i)):
                     if p[ind_out, ind_in] > np.random.random():
                         tot_w = strengths_block[sect_out, sect_in]
                         tmp = s_out*s_in
@@ -312,8 +312,7 @@ def assign_weights_cimi_block_one_z(p, out_strength, in_strength,
 
 
 @jit(nopython=True, parallel=True)
-def vector_fitness_prob_array_block_mult_z(out_strength, in_strength,
-                                               z, N):
+def vector_fitness_prob_array_block_mult_z(out_strength, in_strength, z, N):
     """
     Function computing the Probability Matrix of the Cimi block model
     with just one parameter controlling for the density.
@@ -335,7 +334,7 @@ def vector_fitness_prob_array_block_mult_z(out_strength, in_strength,
     return p
 
 
-#@jit(forceobj=True, parallel=True)
+# @jit(forceobj=True, parallel=True)
 def expected_links_block_mult_z(out_strength, in_strength, z):
     """
     Function computing the expeceted number of links, under the Cimi
@@ -357,6 +356,3 @@ def expected_links_block_mult_z(out_strength, in_strength, z):
                 tmp2 = z[sect_out, sect_in]*tmp
                 p[sect_out, sect_in] += tmp2 / (1 + tmp2)
     return np.reshape(p, -1)
-
-
-
--- tests/test_sample_graph.py
+++ tests/test_sample_graph.py
@@ -73,7 +73,7 @@ class TestStripeFitnessModel():
                                       num_links_tot)
         model.fit(z0=0)
         np.testing.assert_allclose(model.z,
-                                   0.77,
+                                   0.730334,
                                    rtol=1e-6)
 
     def test_solver_multi(self):
@@ -90,12 +90,13 @@ class TestStripeFitnessModel():
         """ Check that the returned probability array is correct. """
         true_value = np.zeros((num_nodes, num_nodes, num_groups),
                               dtype=np.float64)
-        true_value[0, 3, 0] = 1.
-        true_value[1, 0, 1] = 1.
-        true_value[2, 0, 2] = 0.912523
-        true_value[2, 1, 2] = 0.886668
-        true_value[3, 0, 2] = 0.634848
-        true_value[3, 1, 2] = 0.565961
+
+        true_value[0, 3, 0] = 0.744985
+        true_value[1, 0, 1] = 0.948075
+        true_value[2, 0, 2] = 0.946028
+        true_value[2, 1, 2] = 0.929309
+        true_value[3, 0, 2] = 0.744985
+        true_value[3, 1, 2] = 0.686619
 
         model = ge.StripeFitnessModel(out_strength,
                                       in_strength,
@@ -124,10 +125,10 @@ class TestStripeFitnessModel():
 
     def test_probability_matrix_single(self):
         """ Check that the returned probability matrix is the correct one. """
-        true_value = np.array([[0., 0., 0., 1.],
-                              [1., 0., 0., 0.],
-                              [0.912523, 0.886668, 0., 0.],
-                              [0.634848, 0.565961, 0., 0.]])
+        true_value = np.array([[0., 0., 0., 0.744985],
+                              [0.948075, 0., 0., 0.],
+                              [0.946028, 0.929309, 0., 0.],
+                              [0.744985, 0.686619, 0., 0.]])
 
         model = ge.StripeFitnessModel(out_strength,
                                       in_strength,
