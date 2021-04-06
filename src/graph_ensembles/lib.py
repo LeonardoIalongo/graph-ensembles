6b4e2d34bbfeb79818b3c89a90cd763c59671cc2
LeonardoIalongo <leonardo.ialongo@gmail.com>
2021-04-06 18:45:27 +0200

    Added pagerank and propagate methods to lib.

--- src/graph_ensembles/lib.py
+++ src/graph_ensembles/lib.py
@@ -5,6 +5,7 @@
 from . import graphs
 from . import methods as mt
 import numpy as np
+from numba import jit
 from scipy.sparse import coo_matrix
 from numpy.lib.recfunctions import rec_append_fields as append_fields
 import warnings
@@ -66,5 +67,92 @@ def add_groups(g, group_dict):
         g.v = append_fields(g.v, 'group', group_dict)
 
 
-def propagate_measure(g, meas, steps):
-    pass
+@jit(nopython=True)
+def normalise_rows(rows, clms, weights):
+    new_w = np.zeros(len(weights), dtype=weights.dtype)
+    for i in range(len(rows) - 1):
+        n = rows[i]
+        m = rows[i + 1]
+        vals = weights[n:m]
+
+        if (n == m):
+            continue
+
+        new_w[n:m] = vals / np.sum(vals)
+
+    return new_w
+
+
+@jit(nopython=True)
+def propagate_measure(rows, clms, weights, meas, absorb=False):
+    N = len(meas)
+    update = np.zeros(N, dtype=np.float64)
+
+    for i in range(len(rows) - 1):
+        n = rows[i]
+        m = rows[i + 1]
+        clm = clms[n:m]
+        vals = weights[n:m]
+
+        if (n == m):
+            if not absorb:
+                for k in range(N):
+                    update[k] += meas[i]/N
+                    # if k != i:
+                    #     update[k] += meas[i]/(N - 1)
+        else:
+            for k in range(len(clm)):
+                j = clm[k]
+                update[j] += vals[k]*meas[i]
+
+    return update
+
+
+def pagerank(g, alpha=0.85, max_iter=100, tol=1e-6, weighted=True):
+    """ Compute the pagerank for all nodes in the graph.
+
+    Note labelled graphs are compressed, meaning that the link multiplicity
+    is eliminated by summing the weights over labels. If the graph is not
+    weighted then only one link is considered to exist.
+    """
+    # Get adj in csc for fast propagation
+    if isinstance(g, graphs.LabelGraph):
+        adj = g.adjacency_matrix(kind='csr', compressed=True)
+    else:
+        adj = g.adjacency_matrix(kind='csr')
+
+    if not weighted:
+        adj[adj != 0] = 1
+    elif weighted and not isinstance(g, graphs.WeightedGraph):
+        msg = 'Weighted set to true but graph does not have weights.'
+        warnings.warn(msg, UserWarning)
+
+    # Extract data from csr matrix
+    i = adj.indptr
+    j = adj.indices
+    w = adj.data
+
+    # Set each row sum to be equal to one
+    w = normalise_rows(i, j, w)
+
+    # Initialise result
+    N = g.num_vertices
+    rank = np.ones(g.num_vertices, dtype=np.float64) / N
+
+    # Iterate measure propagation
+    for n in range(max_iter):
+        # Compute update
+        new_rank = propagate_measure(i, j, w, rank)
+
+        # Add damping factor
+        new_rank = new_rank*alpha + (1-alpha)/N
+
+        # Check for convergence
+        old_rank = rank
+        rank = new_rank
+        if np.absolute(rank - old_rank).sum() < N*tol:
+            print('Converged in ', n, ' iterations!')
+            return rank
+
+    print('Stopped after ', n + 1, ' iterations!')
+    return rank
