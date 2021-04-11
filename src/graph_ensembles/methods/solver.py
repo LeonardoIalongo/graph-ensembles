064330389973349b78393368ebfe2247852d3f44
LeonardoIalongo <leonardo.ialongo@gmail.com>
2021-04-11 23:08:52 +0200

    Started creating a solver for the case with alpha, incomplete and not working.

--- src/graph_ensembles/methods/solver.py
+++ src/graph_ensembles/methods/solver.py
@@ -234,3 +234,150 @@ def fixed_point_solver(x0, fun, xtol=1e-6, max_iter=100, full_return=False,
                         xtol=xtol)
     else:
         return x
+
+
+def newton_alpha_solver(x0, fun, min_d, tol=1e-6, xtol=1e-6, max_iter=100,
+                        full_return=False, verbose=False):
+    """Find the optimal z and alpha that satisfy the constraints:
+        - Expected number of links == empirical number of links
+        - Mininum non-zero expected degree is at least one
+        - z > 0 and alpha > 0
+
+    Note alpha will be chosen to be the minimum deviation from one that
+    satisfies the constraints.
+
+    Parameters
+    ----------
+    x0: float or np.ndarray
+        starting points of the method
+    fun: function (z, a)
+        function that returns both the value of the function and the Jacobian
+    min_d: function (z, a)
+        function for the minimum degree value
+    tol: float
+        tolerance for the exit condition on the norm
+    xtol: float
+        relative tol for the exit condition on difference between iterations
+    max_iter: int or float
+        maximum number of iteration
+    full_return: boolean
+        if true return all info on convergence
+    verbose: boolean
+        if true print debug info while iterating
+    Returns
+    -------
+    Solution
+        Returns a Solution object.
+    """
+    n_iter = 0
+    x = x0
+    a = 1
+    alpha = 1
+    f, f_x, f_a = fun(x, a)
+    d, d_x, d_a = min_d(x, a)
+    norm = np.abs(f)
+    diff = 1
+
+    # Introduce bounds on x (max x with negative f, min x with positive f)
+    x_p = np.inf
+    x_n = 0
+
+    if verbose:
+        print("x0 = {}".format(x))
+        print("|f(x0)| = {}".format(norm))
+        print("min_d = {}".format(d))
+
+    if full_return:
+        f_seq = [f]
+        x_seq = [x]
+        a_seq = [a]
+        norm_seq = [norm]
+        min_d_seq = [d]
+        diff_seq = [diff]
+        alpha_seq = [1]
+
+    for n_iter in range(max_iter):
+        # Save previous iteration
+        x_old = x
+
+        # Compute update
+        if f_x > tol:
+            dx = - f/f_x
+        else:
+            dx = - f/tol  # Note that H is always positive
+
+        # Update x
+        x = x + alpha * dx
+        if x <= x_n:
+            alpha = alpha / 2
+            x = x_n
+        elif x >= x_p:
+            alpha = alpha / 2
+            x = x_p
+        else:
+            alpha = 1
+
+        # update a
+        if f >= - tol and d < 1:
+            a = a - d / d_x
+        else:
+            a = 1
+
+        # Update objective functions
+        f, f_z, f_a = fun(x, a)
+        d, d_x, d_a = min_d(x, a)
+
+        # Update bounds (how does it work with alpha changing?)
+        # if f > 0 and x < x_p:
+        #     x_p = x
+        # elif f < 0 and x > x_n:
+        #     x_n = x
+
+        # stopping condition computation
+        norm = np.abs(f)
+        if x_old != 0:
+            diff = np.abs((x - x_old)/x_old)
+        else:
+            diff = 1
+
+        if ((norm <= tol or diff > xtol) and d >= 1):
+            break
+
+        if full_return:
+            f_seq.append(f)
+            min_d_seq.append(d)
+            x_seq.append(x)
+            a_seq.append(a)
+            norm_seq.append(norm)
+            diff_seq.append(diff)
+            alpha_seq.append(alpha)
+
+        if verbose:
+            print("    Iteration {}".format(n_iter))
+            print("    fun = {}".format(f))
+            print("    min_d = {}".format(d))
+            print("    fun_prime = {}".format(f_x))
+            print("    fun_prime_alpha = {}".format(f_a))
+            print("    dx = {}".format(dx))
+            print("    x = {}".format(x))
+            print("    alpha = {}".format(alpha))
+            print("    |f(x)| = {}".format(norm))
+            print("    diff = {}".format(diff))
+            print(' ')
+
+    if verbose:
+        print(' ')
+
+    if full_return:
+        return Solution(x, n_iter, max_iter, 'newton',
+                        f_seq=np.array(f_seq),
+                        min_d_seq=np.array(min_d_seq),
+                        x_seq=np.array(x_seq),
+                        a_seq=np.array(a_seq),
+                        norm_seq=np.array(norm_seq),
+                        diff_seq=np.array(diff_seq),
+                        alpha_seq=np.array(alpha_seq),
+                        tol=tol,
+                        xtol=xtol)
+    else:
+        return x
