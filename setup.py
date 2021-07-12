0164421f0193fcd93040a4b9858d2a30b057c2b3
LeonardoIalongo <leonardo.ialongo@gmail.com>
2021-07-12 09:51:44 +0200

    Preparing version 0.2.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author="Leonardo Niccolò Ialongo",
     author_email='leonardo.ialongo@gmail.com',
     python_requires='>=3.0',
-    version='0.1.3',
+    version='0.2.0',
     url='https://github.com/LeonardoIalongo/graph-ensembles',
     description=("The graph ensemble package contains a set of methods to"
                  " build fitness based graph ensembles from marginal"
