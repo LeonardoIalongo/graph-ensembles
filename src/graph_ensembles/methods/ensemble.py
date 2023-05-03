Corrected NLL formula

src/graph_ensembles/methods/ensemble.py
@@ -330,7 +330,7 @@ def fit_f_jac_selfloops(p_f, jac_f, param, fit_out, fit_in):
 @jit(nopython=True)
 def NLL(param, out_in_pairs, remainder):
     """Negative log likelihood of the scale invariant probability function"""
-    func = -np.sum(np.log(1-np.e**(param*out_in_pairs[i][0]*out_in_pairs[i][1])) for i in range(len(out_in_pairs))) 
+    func = -np.sum(np.log(1-exp(-param*out_in_pairs[i][0]*out_in_pairs[i][1])) for i in range(len(out_in_pairs))) 
     + param*(remainder - np.sum(out_in_pairs[i][0]*out_in_pairs[i][1] for i in range(len(out_in_pairs))))
 
     return func