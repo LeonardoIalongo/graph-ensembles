9123be9a52f2e94527c60cd896730794ca4bb950
LeonardoIalongo <leonardo.ialongo@gmail.com>
2023-10-16 20:59:38 +0200

    Started new package structure.

---
+++ src/graph_ensembles/spark/graphs.py
@@ -0,0 +1,6 @@
+""" This module defines the classes of graphs that support the creation of
+ensembles and the exploration of their properties. 
+
+This version stores all properties as spark DataFramse and is suitable for 
+very large graphs. 
+"""
